cef564ee76e53d09aaeb563dd7491bf5743de40f
kresna59 <agungkresna58@gmail.com>
2023-07-16 20:25:42 +0700

    test

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -1,3 +1,13 @@
 @extends('layouts.app')
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>INI BERANDA</h1>
+</body>
+</html>
 @section('content')
